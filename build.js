06acb40203156600362476ac5f7665594a0d563c
Anddward.Liao <Anddward@gmail.com>
2018-07-09 18:24:36 +0800

    fix makefile compile issue
    1. issue description: err while exec make , complain no target
    solve: exec `qmake` first, then exec `make`

--- build.js
+++ build.js
@@ -2,7 +2,8 @@ import shell from 'shelljs'
 import chalk from 'chalk'
 
 const COMMAND_CLR = "rm -rf ./dist"
-const COMMAND_BUILD = "qmake & make"
+const COMMAND_CONFIG= "qmake"
+const COMMAND_BUILD= "make"
 const COMMAND_EXEC = "./dist/cppBoot"
 
 const bar = (msg) => {
@@ -13,13 +14,20 @@ const info = (msg) => {
   console.log(chalk.yellow(msg))
 }
 
-
 bar('clean')
 shell.exec(COMMAND_CLR)
 info('clean complete')
+
+bar('config')
+shell.exec(COMMAND_CONFIG)
+info('config complete')
+
 bar('build')
 shell.exec(COMMAND_BUILD)
 info('build complete')
+
 bar('exec')
 shell.exec(COMMAND_EXEC)
+
+
 
